41a5f7d1e6b2bfacaff1975822d4467ec6854314
assaad salameh <56511035+assaads@users.noreply.github.com>
2024-11-22 16:58:33 +0000

    bug

--- aws-hack/src/app/social-media-post-creator.tsx
+++ aws-hack/src/app/social-media-post-creator.tsx
@@ -8,7 +8,7 @@ import { Checkbox } from "@/components/ui/checkbox"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { Input } from "@/components/ui/input"
-import { useToast } from "@/components/ui/use-toast"
+import { useToast } from "@/hooks/use-toast"
 
 const socialMediaPlatforms = [
   { id: "facebook", name: "Facebook" },

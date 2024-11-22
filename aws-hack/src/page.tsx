750620b45678ae113a8ce3906614eaa441f04fc8
assaad salameh <56511035+assaads@users.noreply.github.com>
2024-11-22 15:48:53 +0000

    fix bug

--- aws-hack/src/page.tsx
+++ aws-hack/src/page.tsx
@@ -1,10 +1,10 @@
-import SocialMediaPostCreator from "./social-media-post-creator"
+// import SocialMediaPostCreator from "./social-media-post-creator"
 
 export default function Page() {
   return (
     <div className="container mx-auto py-10">
       <h1 className="text-3xl font-bold mb-6 text-center">Social Media Post Creator</h1>
-      <SocialMediaPostCreator />
+      {/* <SocialMediaPostCreator /> */}
     </div>
   )
 }
